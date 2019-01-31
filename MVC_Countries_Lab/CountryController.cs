1e787193d9b6d902f57e241b4eca6af7b9e9ac85
Steve Webster <steve.webster190@gmail.com>
2019-01-31 08:42:32 -0500

    initial commit

---
+++ MVC_Countries_Lab/CountryController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_Countries_Lab
+{
+    class CountryController
+    {
+        public void CountryDetails(Country c)
+        {
+            CountryDetails cd = new CountryDetails(c);
+            cd.Display();
+        }
+        public static void CountryMenu()
+        {
+            bool runthis = true;
+            while (runthis)
+            {
+                List<Country> countries;
+                List<string> countryNames = new List<string>();
+                string input = CountryMenuView.UserPick();
+                if (input.ToLower() == "north america")
+                {
+                    NorthAmericaDB nadb = new NorthAmericaDB();
+                    countries = nadb.Countries;
+                    foreach (Country item in countries)
+                    {
+                        countryNames.Add(item.Name);
+                        Console.WriteLine(item.Name);
+                    }
+                    Console.WriteLine("what coutry would you like ot learn about?");
+                    string input2 = Console.ReadLine();
+                    if (input2.ToLower() == "usa")
+                    {
+                        foreach (Country c in countries)
+                        {
+                            if (c.Name.ToLower() == input2)
+                            {
+                                CountryDetails cc = new CountryDetails(c);
+                                cc.Display();
+                            }
+                        }
+                    }
+                    else if (input2.ToLower() == "mexico")
+                    {
+                        foreach (Country c in countries)
+                        {
+                            if (c.Name.ToLower() == input2)
+                            {
+                                CountryDetails cc = new CountryDetails(c);
+                                cc.Display();
+                            }
+                        }
+                    }
+                    else if (input2.ToLower() == "Canada")
+                    {
+                        foreach (Country c in countries)
+                        {
+                            if (c.Name.ToLower() == input2)
+                            {
+                                CountryDetails cc = new CountryDetails(c);
+                                cc.Display();
+                            }
+                        }
+                    }
+                    break;
+                }
+                else if (input.ToLower() == "asia")
+                {
+                    AsiaDB adb = new AsiaDB();
+                    countries = adb.Countries;
+                    foreach (Country c in countries)
+                    {
+                        countryNames.Add(c.Name);
+                        Console.WriteLine(c.Name);
+                    }
+                    Console.WriteLine("what coutry would you like ot learn about?");
+                    string input2 = Console.ReadLine();
+                    if (input2.ToLower() == "vietnam")
+                    {
+                        foreach (Country c in countries)
+                        {
+                            if (c.Name.ToLower() == input2)
+                            {
+                                CountryDetails cc = new CountryDetails(c);
+                                cc.Display();
+                            }
+                        }
+                    }
+                    else if (input2.ToLower() == "japan")
+                    {
+                        foreach (Country c in countries)
+                        {
+                            if (c.Name.ToLower() == input2)
+                            {
+                                CountryDetails cc = new CountryDetails(c);
+                                cc.Display();
+                            }
+                        }
+                    }
+                    else if (input2.ToLower() == "north korea")
+                    {
+                        foreach (Country c in countries)
+                        {
+                            if (c.Name.ToLower() == input2)
+                            {
+                                CountryDetails cc = new CountryDetails(c);
+                                cc.Display();
+                            }
+                        }
+                    }
+                    else if (input2.ToLower() == "south korea")
+                    {
+                        foreach (Country c in countries)
+                        {
+                            if (c.Name.ToLower() == input2)
+                            {
+                                CountryDetails cc = new CountryDetails(c);
+                                cc.Display();
+                            }
+                        }
+                    }
+                    break;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+        }
+    }
+}
---
+++ MVC_Countries_Lab/CountryMenu.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_Countries_Lab
+{
+    class CountryMenuView
+    {
+        public static string UserPick()
+        {
+            Console.WriteLine("What contient would you like to learn about");
+            string input = Console.ReadLine();
+            return input;
+        }
+
+    }
+}
